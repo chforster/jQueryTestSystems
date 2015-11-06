0356faa633c3d344241af99aafb8d06f56bf4bc1
Christoph Forster <christoph.forster@gmail.com>
2015-11-06 04:04:51 -0800

    Mocha TEst

---
+++ mocha/test_01.js
@@ -0,0 +1,13 @@
+/**
+ * Created by devebiz on 11/6/15.
+ */
+/*global
+describe
+*/
+var assert = require('chai').assert;
+
+describe("This is my Test", function () {
+   it("Sollte ...", function () {
+       assert.equal(2,2, "2 gleich 2");
+   });
+});
