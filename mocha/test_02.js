70309d059d2ac2a99fa61d5cfcb04c2e05a5089e
Christoph Forster <christoph.forster@gmail.com>
2015-11-06 04:13:51 -0800

    expect.js

---
+++ mocha/test_02.js
@@ -0,0 +1,13 @@
+/**
+ * Created by devebiz on 11/6/15.
+ */
+/*global
+ describe
+ */
+var expect = require('expect.js');
+
+describe("This is my Test", function () {
+    it("Sollte ...", function () {
+        expect(2).equal(2, "2 gleich 2");
+    });
+});
